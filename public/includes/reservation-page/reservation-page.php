1d2882aaaf8437f1483bfafe9a85f45e01055170
Aurel-2 <aurel4E554C4C@proton.me>
2025-03-24 17:43:51 +0100

    Simple page de réservation

---
+++ public/includes/reservation-page/reservation-page.php
@@ -0,0 +1,27 @@
+<main>
+    <div class="container">
+        <h1>Réservation</h1>
+        <form id="reservation-form">
+            <label for="name">Nom</label>
+            <input type="text" id="name" name="name">
+            <div id="name-error" class="error-message"></div>
+            <label for="date">Date</label>
+            <input type="date" name="date" id="date">
+            <div id="date-error" class="error-message"></div>
+            <label for="time">Heure</label>
+            <select id="time" name="time">
+                <option value="" disabled selected>Choisir une heure</option>
+                <option value="19:00">19:00</option>
+                <option value="20:00">20:00</option>
+                <option value="21:00">21:00</option>
+                <option value="22:00">22:00</option>
+            </select>
+            <div id="time-error" class="error-message"></div>
+            <label for="guests">Nombre de personnes</label>
+            <input type="number" id="guests" name="guests">
+            <div id="guests-error" class="error-message"></div>
+            <button type="submit">Réserver</button>
+        </form>
+    </div>
+</main>
+<script type="module" src="includes/reservation-page/reservation.js"></script>
---
+++ public/reservation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Le menu</title>
+    <link rel="stylesheet" href="css/reservation.css">
+    <link rel="icon" type="image/png" href=" ./resources/images/favicon.png">
+</head>
+<body>
+<?php include_once "includes/header/header.php" ?>
+<?php include_once "includes/reservation-page/reservation-page.php"?>
+<?php include_once "includes/footer/footer.php"?>
+</body>
+</html>
