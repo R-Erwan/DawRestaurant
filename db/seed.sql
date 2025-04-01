061f2c850cd0294813570d8299a6d63dd7704d15
erwan <erwanroussel75@gmail.com>
2025-04-01 20:14:40 +0200

    Hotfix : adresse mail valide pour nobody

--- db/seed.sql
+++ db/seed.sql
@@ -2,5 +2,5 @@ INSERT INTO roles (name) VALUES ('admin'), ('moderator'), ('user');
 INSERT INTO users (name,email,password) VALUES ('admin','admin@leresto.com','$2y$10$fZrsVnaHScn996ZFJ6.n4.MTmXaU/BH3zU/2ai8/Xm2p.bnQgxHHa');
 INSERT INTO user_roles (user_id, role_id) VALUES (1,1);
 
-INSERT INTO users(name,email,password) VALUES ('nobody','nobody','$2y$10$zWJfL/qnuRN0EaXe/a7q8u/QTRisjsruY4vj5mLVKCrBHcDM5Mlsy');
+INSERT INTO users(name,email,password) VALUES ('nobody','nobody@leresto.com','$2y$10$zWJfL/qnuRN0EaXe/a7q8u/QTRisjsruY4vj5mLVKCrBHcDM5Mlsy');
 INSERT INTO user_roles (user_id, role_id) VALUES (2,3);
