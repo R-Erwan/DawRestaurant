d957fc615cc26b7e2fb143909d3aba0a7765c558
erwan <erwanroussel75@gmail.com>
2025-04-02 17:55:18 +0200

    Display admin page front pour les annonces. Reste le dragable et les requetes API.

--- public/includes/home-page/home-page.php
+++ public/includes/home-page/home-page.php
@@ -22,36 +22,7 @@
 </div>
 
 <div class="announce-container">
-    <div class="announce announce-text">
-        <h3>Edward, notre cuisinier distingué</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum lorem sollicitudin purus mattis semper.
-        Phasellus convallis, velit a placerat tempor, magna velit egestas neque, et placerat metus metus ut justo.
-        Etiam urna metus, euismod rutrum elit eu, feugiat finibus eros. Integer a egestas nulla, vitae varius tellus.
-        Nam feugiat nibh at pellentesque posuere. Nunc ac molestie ex. Nullam sed tempus mi.
-        Sed non ante posuere, placerat ex eget, tempus erat. Maecenas quis nulla in justo aliquam luctus.
-            Etiam tincidunt gravida ligula sit amet accumsan. Aenean in magna sed sem faucibus varius.</p>
-    </div>
-    <div class="announce announce-img"><img src="resources/images/cook.jpg" alt="Un cuisinier"></div>
-    <div class="announce announce-img"><img src="resources/images/vege.jpg" alt="Un plat végétarien"></div>
-    <div class="announce announce-text">
-        <h3>Le végétarien sublimé</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum lorem sollicitudin purus mattis semper.
-        Phasellus convallis, velit a placerat tempor, magna velit egestas neque, et placerat metus metus ut justo.
-        Etiam urna metus, euismod rutrum elit eu, feugiat finibus eros. Integer a egestas nulla, vitae varius tellus.
-        Nam feugiat nibh at pellentesque posuere. Nunc ac molestie ex. Nullam sed tempus mi.
-        Sed non ante posuere, placerat ex eget, tempus erat. Maecenas quis nulla in justo aliquam luctus.
-            Etiam tincidunt gravida ligula sit amet accumsan. Aenean in magna sed sem faucibus varius.</p>
-    </div>
-    <div class="announce announce-text">
-        <h3>Des recettes traditionnelles revisitées</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum lorem sollicitudin purus mattis semper.
-        Phasellus convallis, velit a placerat tempor, magna velit egestas neque, et placerat metus metus ut justo.
-        Etiam urna metus, euismod rutrum elit eu, feugiat finibus eros. Integer a egestas nulla, vitae varius tellus.
-        Nam feugiat nibh at pellentesque posuere. Nunc ac molestie ex. Nullam sed tempus mi.
-        Sed non ante posuere, placerat ex eget, tempus erat. Maecenas quis nulla in justo aliquam luctus.
-            Etiam tincidunt gravida ligula sit amet accumsan. Aenean in magna sed sem faucibus varius.</p>
-    </div>
-    <div class="announce announce-img"><img src="resources/images/food3.jpg" alt="Plusieurs plats avec de la nourriture"></div>
+
 </div>
 </main>
 <script src="https://kit.fontawesome.com/c4155bf45a.js" crossorigin="anonymous"></script>
